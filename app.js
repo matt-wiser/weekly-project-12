32983a21f2260b17b959c781642cc7419d61fc2e
Matthew Wiser <matt.wiser@icloud.com>
2022-07-12 21:34:11 -0500

    added end command to stop program hang after user input

--- app.js
+++ app.js
@@ -3,50 +3,78 @@ const cTable = require("console.table");
 const db = require('./db/connection');
 
 const {initialQuestions, employeeQuestions, roleQuestions, departmentQuestions} = require('./lib/questionArrays');
-const { ui } = require('inquirer');
-const { prompt } = require('inquirer');
 
 
 function init(){
     inquirer.prompt(initialQuestions)
     .then(response =>{
-        if (response.actionSelection === 'View all departments') {
-            // Display all departments
+        if (response.action === 'View all departments') {
+            const query = `SELECT * FROM departments`;
+
+            db.query(query, (err, rows) => {
+                if (err) {
+                    console.log(err);
+                }
+                console.table(rows);
+            });
+            db.end();
         } 
-        if (response.actionSelection === 'View all roles') {
-            // Display all roles
+        if (response.action === 'View all roles') {
+            const query = `SELECT * FROM roles`;
+
+            db.query(query, (err, rows) => {
+                if (err) {
+                    console.log(err);
+                }
+                console.table(rows);
+            });
         } 
-         if (response.actionSelection === 'View all employees') {
-            // Display all employees
+         if (response.action === 'View all employees') {
+            const query = `SELECT * FROM employees`;
+
+            db.query(query, (err, rows) => {
+                if (err) {
+                    console.log(err);
+                }
+                console.table(rows);
+            });
+            db.end();
         } 
-        if (response.actionSelection === 'Create a deparment') {
+        if (response.action === 'Create a deparment') {
             inquirer.prompt(departmentQuestions)
             .then(response => {
                 // createNewDepartment(response)
                 console.log(response);
+                db.close();
             });
+            db.end();
         } 
-        if (response.actionSelection === 'Create a role') {
+        if (response.action === 'Create a role') {
             inquirer.prompt(roleQuestions)
             .then(response => {
                 
             })
+            db.end();
         } 
-        if (response.actionSelection === 'Create an employee') {
+        if (response.action === 'Create an employee') {
             inquirer.prompt(employeeQuestions)
             .then(response => {
 
             })
+            db.end();
         } 
-        if (response.actionSelection === 'Update an employee role'){
+        if (response.action === 'Update an employee role'){
             // Edit an employees role
+            db.end();
         }
-        else if (response.actionSelection === 'Exit the program') {
+        if (response.action === 'Exit the program') {
             //exit application
             console.log("GOODBYE");
+            db.end();
         }
     });
 }
+
 init();
 
 
